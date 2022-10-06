fcdaa08bd04e7282d87315a9ebfa0bcd116438b7
Elijah Frechette <LE1837182@crc-lennox.qc.ca>
2022-10-06 08:45:08 -0400

    Commit to fix merge issue

---
+++ PersonTests/Person_LOCAL_179.cs
@@ -0,0 +1,48 @@
+﻿namespace PersonTests
+{
+    /// <summary>
+    /// Represents one person saving their name.
+    /// </summary>
+    public class Person : IMeasurable, IResettable
+    {
+        private int _size;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// Constructor for a person optionally adding their name.
+        /// </summary>
+        /// <param name="name">Person's name, empty by default.</param>
+        public Person(string name = "")
+        {
+            this.Name = name.Trim();
+            _size = GetTheNameSize(name);
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the person.
+        /// </summary>
+        public virtual string Name { get; set; }
+
+
+        public int Size { get { return _size; } }
+
+        /// <summary>
+        /// Returns a string that represents the person.
+        /// </summary>
+        /// <returns>String that represents the person.</returns>
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        public void Reset()
+        {
+            Name = String.Empty;
+        }
+
+        private int GetTheNameSize(string name)
+        {
+            return name.Length;
+        }
+    }
+}
---
+++ PersonTests/Person_REMOTE_179.cs
@@ -0,0 +1,50 @@
+﻿namespace PersonTests
+{
+    /// <summary>
+    /// Represents one person saving their name.
+    /// </summary>
+    public class Person : IDisplayable, IComparable<Person>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// Constructor for a person optionally adding their name.
+        /// </summary>
+        /// <param name="name">Person's name, empty by default.</param>
+        public Person(string name = "")
+        {
+            this.Name = name.Trim();
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the person.
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// Returns a string that represents the person.
+        /// </summary>
+        /// <returns>String that represents the person.</returns>
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        public void Display()
+        {
+            MessageBox.Show(this.Name);
+        }
+
+        public int CompareTo(Person person)
+        {
+
+            if(person.Name == this.Name)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
